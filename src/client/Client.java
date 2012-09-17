06f5590856e1c9504500c6cc5971d95677213162
Joshua Scott <joshua.scott.132@gmail.com>
2012-09-17 22:22:29 +1200

    Added arctechture of supposed gamestates
    Empty with no functionality as of yet, but will add this  as needed

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,15 @@
 package client;
 
 public class Client {
-
+	
+	//current state that determines actions
+	private GameState state;
+	
+	public Client(){
+		state = new LogInState();
+	}
+	
+	public GameState getState(){
+		return state;
+	}
 }
