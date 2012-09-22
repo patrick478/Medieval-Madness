80253ceb99a30214304874191ba6d5e26de326ae
Patrick Barnes <patrick478@gmail.com>
2012-09-23 11:53:21 +1200

    Added simple sound playback. Need to add looping still.

---
+++ src/soundEngine/LoopSound.java
@@ -0,0 +1,18 @@
+package soundEngine;
+
+import java.net.URL;
+import javax.sound.sampled.*;
+
+public class LoopSound {
+
+  public static void main(String[] args) throws Exception {
+    URL url = new URL(
+      "Daybreak.wav");
+    Clip clip = AudioSystem.getClip();
+    AudioInputStream ais = AudioSystem.
+      getAudioInputStream( url );
+    clip.open(ais);
+    clip.loop(5);
+    javax.swing.JOptionPane.showMessageDialog(null, "Close to exit!");
+  }
+} 
