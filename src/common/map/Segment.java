691057b02b2c66158493e93292668cd479265c4b
Ben Allen <benos.9k@gmail.com>
2012-09-24 17:28:30 +1200

    fixed segment normal generation

--- src/common/map/Segment.java
+++ src/common/map/Segment.java
@@ -16,34 +16,31 @@ public class Segment {
 	public static final int vertScale = 8;
 	public static final int horzScale = 2;
 	private static final int terr_tex_size = 256;
-	
+
 	private float[][] heightmap = null;
-	
+
 	private final long xPos;
 	private final long zPos;
-	
-	public Segment(long _xPos, long _zPos, float[][] heightmap)
-	{
+
+	public Segment(long _xPos, long _zPos, float[][] heightmap) {
 		xPos = _xPos;
 		zPos = _zPos;
-		
+
 		this.heightmap = heightmap;
 	}
-	
-	public float[][] getData()
-	{
+
+	public float[][] getData() {
 		return this.heightmap;
 	}
-	
-	public MeshContext getMeshContext(){
+
+	public MeshContext getMeshContext() {
 		Mesh terr_mesh = new Mesh();
-		
+
 		terr_mesh.add(highLOD());
-		
-		
+
 		Material terr_mtl = new Material(new Color(0.4f, 0.4f, 0.4f), new Color(0.1f, 0.1f, 0.1f), 1f);
 		Texture terr_tx = Initial3D.createTexture(terr_tex_size);
-		//populate the terrain texture
+		// populate the terrain texture
 		for (int u = 0; u < terr_tex_size; u++) {
 			for (int v = 0; v < terr_tex_size; v++) {
 				terr_tx.setPixel(u, v, 1f, 0.3f, (float) (Math.random() * 0.4 + 0.3), 0.3f);
@@ -51,42 +48,43 @@ public class Segment {
 		}
 		terr_tx.composeMipMaps();
 		terr_tx.useMipMaps(true);
-		
+
 		terr_mtl = new Material(terr_mtl, terr_tx, null, null);
-		
+
 		MeshContext terr_mc = new MeshContext(terr_mesh, terr_mtl);
-		
+
 		return terr_mc;
 	}
-	
-	private MeshLOD highLOD(){
-		
-		MeshLOD mLOD = new MeshLOD(size * size * 2*2, 3, (size+1) * (size+1)*2, 20, (size+1) * (size+1)*2, 1);
+
+	private MeshLOD highLOD() {
+
+		MeshLOD mLOD = new MeshLOD(size * size * 2 * 2, 3, (size + 1) * (size + 1) * 2, 20,
+				(size + 1) * (size + 1) * 2, 1);
 
 		int[][] ind = new int[(size + 2)][(size + 2)];
-		
-		//first create vectors and normals
+
+		// first create vectors and normals
 		for (int z = 1; z <= size + 1; z++) {
 			for (int x = 1; x <= size + 1; x++) {
-				float h = heightmap[x][z];
-				
-				ind[x-1][z-1] = mLOD.addVertex(x * horzScale + (xPos * size * horzScale), h * vertScale, z * horzScale + (zPos * size * horzScale));
+				float h = heightmap[x][z] * vertScale;
+
+				ind[x - 1][z - 1] = mLOD.addVertex(x * horzScale + (xPos * size * horzScale), h, z * horzScale
+						+ (zPos * size * horzScale));
 
 				// try to construct normal from noise pseudo-derivative
-				Vec3 d0 = Vec3.create(-1, heightmap[x-1][z]- h, 0).unit();
-				Vec3 d1 = Vec3.create(0, heightmap[x][z-1]- h, -1).unit();
-				Vec3 d2 = Vec3.create(1, heightmap[x+1][z] - h, 0).unit();
-				Vec3 d3 = Vec3.create(0, heightmap[x][z+1] - h, 1).unit();
+				Vec3 d0 = Vec3.create(-2, heightmap[x - 1][z] * vertScale - h, 0).unit();
+				Vec3 d1 = Vec3.create(0, heightmap[x][z + 1] * vertScale - h, 2).unit();
+				Vec3 d2 = Vec3.create(2, heightmap[x + 1][z] * vertScale - h, 0).unit();
+				Vec3 d3 = Vec3.create(0, heightmap[x][z - 1] * vertScale - h, -2).unit();
 
-				Vec3 normal = (d0.cross(d1).add(d1.cross(d2)).add(d2.cross(d3)).add(d3.cross(d0))).neg().unit();
+				Vec3 normal = (d0.cross(d1).add(d1.cross(d2)).add(d2.cross(d3)).add(d3.cross(d0))).unit();
 
 				mLOD.addNormal(normal.x, normal.y, normal.z);
-				
-//				mLOD.addNormal(0, 1, 0);
+
+				// mLOD.addNormal(0, 1, 0);
 			}
 		}
-		
-		
+
 		Random r = new Random(32);
 
 		int[] tri_vt_1a = new int[] { mLOD.addTexCoord(0, 1), mLOD.addTexCoord(1, 1), mLOD.addTexCoord(0, 0) };
