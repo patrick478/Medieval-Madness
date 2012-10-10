f2904d980d5c4b8cae0b096b8e58bcc31da066b6
Patrick Barnes <patrick478@gmail.com>
2012-10-10 22:45:14 +1300

    Changed menu to use individual arrow icons, rather than simply changing the whole image being displayed

--- src/game/states/MainMenuGUIState.java
+++ src/game/states/MainMenuGUIState.java
@@ -26,12 +26,16 @@ import game.GameState;
 public class MainMenuGUIState extends GameState {
 
 	static MouseArea joinArea = new MouseArea(267, 345, 260, 35);
-	static MouseArea startArea = new MouseArea(235, 407, 571-135, 35);
+	static MouseArea startArea = new MouseArea(235, 407, 571-235, 35);
 	Picture pic;
+	Picture pleftArrow;
+	Picture prightArrow;
 	BufferedImage mainmenu = null;
-	BufferedImage joinmenu = null;
-	BufferedImage startmenu = null;
+	//BufferedImage joinmenu = null;
+	//BufferedImage startmenu = null;
+	Picture leftTop, leftBottom, rightTop, rightBottom;
 
+	
 
 	private final ActionListener l = new ActionListener() {
 
@@ -50,13 +54,25 @@ public class MainMenuGUIState extends GameState {
 			}
 			else if(e.getActionCommand().equals("mouseover")){
 				if(e.getSource().equals(joinArea)){
-					pic.setPicture(joinmenu);
+				//	pic.setPicture(joinmenu);
+					leftTop.setVisible();
+					rightTop.setVisible();
+					leftBottom.setHidden();
+					rightBottom.setHidden();
+					
+					repaint();
+					
 				}
 					
 
 				else if(e.getSource().equals(startArea)){
-					pic.setPicture(startmenu);
-
+					//pic.setPicture(startmenu);
+					leftTop.setHidden();
+					rightTop.setHidden();
+					leftBottom.setVisible();
+					rightBottom.setVisible();
+					
+					repaint();
 				}
 					
 
@@ -79,14 +95,26 @@ public class MainMenuGUIState extends GameState {
 
 		try {
 			mainmenu = ImageIO.read(new File("resources/mainmenu.png"));
-			joinmenu = ImageIO.read(new File("resources/mainmenujoin.png"));
-			startmenu = ImageIO.read(new File("resources/mainmenustart.png"));
+			//joinmenu = ImageIO.read(new File("resources/mainmenujoin.png"));
+			//startmenu = ImageIO.read(new File("resources/mainmenustart.png"));
+			BufferedImage rightArrow = ImageIO.read(new File("resources/rightarrow.png"));
+			BufferedImage leftArrow = ImageIO.read(new File("resources/leftarrow.png"));
+			leftTop = new Picture(leftArrow, 240, 345,35,35);
+			leftBottom = new Picture(leftArrow, 200, 407,35,35);
+			rightTop = new Picture(rightArrow, 525, 345,35,35);
+			rightBottom = new Picture(rightArrow, 570, 407,35,35);
 
+			
 
 		} catch (IOException e) {
 			throw new AssertionError(e);
 		}
-		pic = new Picture(mainmenu, 800,600);
+		
+		leftTop.setHidden();
+		rightTop.setHidden();
+		leftBottom.setHidden();
+		rightBottom.setHidden();
+		pic = new Picture(mainmenu,0,0, 800,600);
 		p.getRoot().add(pic);
 		//input from user
 
@@ -94,7 +122,11 @@ public class MainMenuGUIState extends GameState {
 		p.getRoot().add(joinArea);
 		startArea.addActionListener(l);
 		p.getRoot().add(startArea);
-
+		p.getRoot().add(leftTop);
+		p.getRoot().add(rightTop);
+		p.getRoot().add(leftBottom);
+		p.getRoot().add(rightBottom);
+	
 		scene.addDrawable(p);
 
 		p.requestVisible(true);
@@ -102,7 +134,13 @@ public class MainMenuGUIState extends GameState {
 
 	}
 
-
+	public void repaint(){
+		pic.repaint();
+		leftTop.repaint();
+		rightTop.repaint();
+		leftBottom.repaint();
+		rightBottom.repaint();
+	}
 
 	@Override
 	public void update(double delta) {
